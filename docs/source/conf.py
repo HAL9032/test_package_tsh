d20e2dda0c0422043ae139ac33008f456d8c4477
Patrick Hanel <47063786+HAL9032@users.noreply.github.com>
2024-07-26 20:47:43 +0200

    removed custom CSS

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -36,7 +36,6 @@ exclude_patterns = []
 
 html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
-html_css_files = ['custom.css']
 
 autosummary_generate = True
 nbsphinx_notebook_dir = '../tutorials'
